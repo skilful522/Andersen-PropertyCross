fd4b79305f421f6c3d02db408cca9f04b3b2a0bc
Stanislav <skilful522@gmail.com>
2019-12-06 15:23:36 +0300

    add postcss, cssModule

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-url'),
+        require('postcss-preset-env')({
+            browsers: 'last 3 versions',
+            stage: 0,
+        }),
+    ],
+};
